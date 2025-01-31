9d476b60effc05e698b3d0baa5a1c8f63f328d21
Taylor Curran <taylorcurranagtc@gmail.com>
2025-01-30 18:43:42 -0600

    queries work

---
+++ src/get_gong_data/refresh_gong_from_bq.py
@@ -0,0 +1,26 @@
+from prefect import task, flow
+from google.cloud import bigquery
+from dotenv import load_dotenv
+import os
+from queries import transcript_query
+
+
+@flow
+def refresh_gong_transcripts(limit: int = 100):
+    """
+    Get the transcript data from Gong
+    """
+    load_dotenv()
+    gcp_project_id = os.getenv("GCP_PROJECT_ID")
+
+    client = bigquery.Client(project=gcp_project_id)
+
+    query = transcript_query + f"LIMIT {limit};"
+    rows = client.query_and_wait(query)
+
+    for row in rows:
+        print(dict(row))
+
+
+if __name__ == "__main__":
+    refresh_gong_transcripts(2)
