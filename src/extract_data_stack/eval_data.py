db4d9565ed1fefedf432817765d274859f02baa7
Taylor Curran <taylorcurranagtc@gmail.com>
2025-02-23 15:37:42 -0600

    ignoring eval data

--- src/extract_data_stack/eval_data.py
+++
@@ -1,9 +0,0 @@
-from main import TechStack
-
-us_soccer_stack = TechStack(
-    primary_previous_solution=OrchestrationTool.HOME_GROWN_BASIC,
-    secondary_previous_solutions=[OrchestrationTool.AWS_LAMBDA_FUNCTIONS],
-    cloud_provider="AWS",
-)
-
-training_data = {"006Rm00000QuHC6IAN": us_soccer_stack}
